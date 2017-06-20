52ad0348050d62ea2f76bbf29cf07e49c84a15b7
sverweij <sverweij@users.noreply.github.com>
2017-06-20 21:06:10 +0200

    :mag: also pass a path in the test

--- spec/renderGraphViz-spec.js
+++ spec/renderGraphViz-spec.js
@@ -4,29 +4,29 @@ import render from '../lib/renderGraphViz';
 
 describe("renderGraphViz", () => {
     it("renders a valid program as an svg", () => {
-        render('graph { a -- b [label="okiedokie"]}', (err, ok) => {
+        render('graph { a -- b [label="okiedokie"]}', "", (err, ok) => {
             expect(err).toBe(null && expect(ok).toContain('<svg '));
         });
     });
 
     it("returns an error on an invalid program", () => {
-        render('this is an invalid program', (err, ok) => {
+        render('this is an invalid program', "", (err, ok) => {
             expect(err.message).toContain("Error: syntax error in line 1 near 'this'");
             expect(ok).toBeUndefined();
         });
     });
 
     it("recovers from an error viz.js doesn't recover from", () => {
-        render('graph {a --b [label="unrecoverable]}', (err, ok) => {
+        render('graph {a --b [label="unrecoverable]}', "", (err, ok) => {
             expect(err.message).toContain("Error: syntax error in line 1 scanning a quoted string (missing");
             expect(ok).toBeUndefined();
-            render('graph {a --b [label="unrecoverable]}', (err1, ok1) => {
+            render('graph {a --b [label="unrecoverable]}', "", (err1, ok1) => {
                 expect(err1.message).toContain("Error: syntax error in line 1 scanning a quoted string (missing");
                 expect(ok1).toBeUndefined();
-                render('graph {a --b [label="unrecoverable]}', (err2, ok2) => {
+                render('graph {a --b [label="unrecoverable]}', "", (err2, ok2) => {
                     expect(err2.message).toContain("Error: syntax error in line 1 scanning a quoted string (missing");
                     expect(ok2).toBeUndefined();
-                    render('graph {a --b [label="unrecoverable"]}', (err3, ok3) => {
+                    render('graph {a --b [label="unrecoverable"]}', "", (err3, ok3) => {
                         expect(ok3).toContain('<svg ');
                         expect(err3).toBe(null);
                     });
